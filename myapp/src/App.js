dee68d39c84333ea319669120e64d7a208db8803
Todd Croak-Falen <Todd Croak-Falen>
2016-11-11 15:17:05 -0800

    Revised text and changed React logo to NYT logo.

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,22 +1,24 @@
 import React, { Component } from 'react';
 import {Router, Route, browserHistory} from 'react-router';
-import logo from './logo.svg';
+import logo from './nyt_logo.png';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<div className="App-header">
+					<img src={logo} className="App-logo" alt="logo" />
+					<h2>New York Times Article Scrubber</h2>
+					
+					<h3>Search for and annotate articles of interest.</h3>
+				</div>
+				<p className="App-intro">
+					To get started, edit <code>src/App.js</code> and save to reload.
+				</p>
+			</div>
+		);
+	}
 }
 
 export default App;
